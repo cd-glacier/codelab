d39f13fd4eab88bc7e68e94ef9a54c32d44b68ee
cd-glacier <hyoga0216@gmail.com>
2021-07-11 20:31:31 +0900

    Replace coroutines

--- kotlin-coroutines/coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
+++ kotlin-coroutines/coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
@@ -19,6 +19,9 @@ package com.example.android.kotlincoroutines.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
 import com.example.android.kotlincoroutines.util.BACKGROUND
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 /**
  * TitleRepository provides an interface to fetch a title or request a new one be generated.
@@ -50,28 +53,24 @@ class TitleRepository(val network: MainNetwork, val titleDao: TitleDao) {
      * This method does not return the new title. Use [TitleRepository.title] to observe
      * the current tile.
      */
-    fun refreshTitleWithCallbacks(titleRefreshCallback: TitleRefreshCallback) {
-        // This request will be run on a background thread by retrofit
-        BACKGROUND.submit {
-            try {
-                // Make network request using a blocking call
-                val result = network.fetchNextTitle().execute()
-                if (result.isSuccessful) {
-                    // Save it to database
-                    titleDao.insertTitle(Title(result.body()!!))
-                    // Inform the caller the refresh is completed
-                    titleRefreshCallback.onCompleted()
-                } else {
-                    // If it's not successful, inform the callback of the error
-                    titleRefreshCallback.onError(
-                            TitleRefreshError("Unable to refresh title", null))
-                }
+    suspend fun refreshTitleWithCallbacks(titleRefreshCallback: TitleRefreshCallback) {
+        withContext(Dispatchers.IO) {
+            val result = try {
+                network.fetchNextTitle().execute()
             } catch (cause: Throwable) {
-                // If anything throws an exception, inform the caller
-                titleRefreshCallback.onError(
-                        TitleRefreshError("Unable to refresh title", cause))
+                throw TitleRefreshError("Unable to refresh title", cause)
+            }
+            if (result.isSuccessful) {
+                titleDao.insertTitle(Title(result.body()!!))
+                titleRefreshCallback.onCompleted()
+            } else {
+                throw TitleRefreshError("Unable to refresh title", null)
             }
         }
+    }
+
+    suspend fun refreshTitle() {
+        delay(500)
     }
 }
 
